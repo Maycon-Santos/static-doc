9dda4881d8801a6952d6e70b8e6f4ca63e93ac4f
Maycon-Santos <mayconsantos.dev@gmail.com>
2020-08-25 10:43:08 -0300

    Upgrade build commands

--- packages/default-theme/gulpfile.js
+++ packages/default-theme/gulpfile.js
@@ -1,5 +1,6 @@
 const postcss = require('gulp-postcss')
 const concat = require('gulp-concat')
+const babel = require('gulp-babel')
 const gulp = require('gulp')
 
 function transformCss () {
@@ -9,8 +10,18 @@ function transformCss () {
     .pipe(gulp.dest('./dist'))
 }
 
+function transformTs () {
+  return gulp.src(['./theme/**/*.ts', './theme/**/*.tsx'])
+    .pipe(babel())
+    .pipe(gulp.dest('./dist'))
+}
+
 if (process.argv.includes('--watch')) {
   gulp.watch(['./theme/**/*.css'], transformCss)
 }
 
-exports.default = transformCss
+if (process.argv.includes('--watch')) {
+  gulp.watch(['./theme/**/*.ts', './theme/**/*.tsx'], transformTs)
+}
+
+exports.default = gulp.parallel(transformTs, transformCss)
